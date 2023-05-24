87e8b972a3175d95215c60e04b876028eb4b7448
ayatbd <albehestione@gmail.com>
2023-05-24 15:49:29 +0600

    first commit

---
+++ src/firebase/firebase.config.js
@@ -0,0 +1,19 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
+  apiKey: "AIzaSyBv7f1uKRsreshzzXxtRu-92svJV0IpYn4",
+  authDomain: "chef-hero-c54ac.firebaseapp.com",
+  projectId: "chef-hero-c54ac",
+  storageBucket: "chef-hero-c54ac.appspot.com",
+  messagingSenderId: "347491020043",
+  appId: "1:347491020043:web:c3524923fcd1369b1a460e"
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
+
+export default app;
